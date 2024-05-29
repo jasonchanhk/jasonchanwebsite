cb911f0cc22a4e7dd464e9e4d360e34e1bd4dd8a
Jason Chan <jasonchan@Jasons-MacBook-Air.local>
2024-05-29 14:13:10 +0100

    create contact page & send email feature

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import RedirectButton from './redirectButton'
 import Link from 'next/link'
@@ -7,6 +7,9 @@ import { FiGithub, FiLinkedin } from 'react-icons/fi'
 import { IoLocationSharp } from "react-icons/io5";
 import { MdEmail } from 'react-icons/md'
 import { IconContext } from "react-icons";
+
+import emailjs from '@emailjs/browser';
+import { copyEvent } from '../utils/copyEvent'
 
 const Contact = () => {
     return (
@@ -17,25 +20,13 @@ const Contact = () => {
                     <Invitation />
                     <ContactList />
                 </div>
-                <div className='flex-1 text-black text-lg p-2d'>
-                    <div className='grid md:grid-cols-2 gap-5'>
-                        <input type="text" placeholder='First Name' className='bg-gray-50 p-5 rounded-2xl' />
-                        <input type="text" placeholder='Last Name' className='bg-gray-50 p-5 rounded-2xl' />
-                        <input type="text" placeholder='Email' className='bg-gray-50 p-5 rounded-2xl' />
-                        <input type="text" placeholder='Phone Number' className='bg-gray-50 p-5 rounded-2xl' />
-                        <input type="text" placeholder='Subject' className='bg-gray-50 col-span-2 p-5 rounded-2xl' />
-                        <textarea name="" id="" placeholder='Message' className='bg-gray-50 col-span-2 p-5 rounded-2xl h-52' ></textarea>
-                    </div>
-
-                    <RedirectButton className='mt-10 bg-lime-400'>
-                        Send Message
-                    </RedirectButton>
+                <div className='flex-1 text-black text-lg p-2 flex flex-col justify-end'>
+                    <MessageForm />
                 </div>
             </div>
         </div>
     )
 }
-
 
 const Invitation = () => {
     return (
@@ -70,10 +61,10 @@ const ContactTitle = () => {
 
 const ContactList = () => {
     return (
-        <IconContext.Provider value={{ color: "white", size: "1.2em", className: "cursor-pointer" }}>
-            <div className='flex flex-col gap-4 pt-10 md:self-end '>
+        <IconContext.Provider value={{ color: "white", size: "1.2em" }}>
+            <div className='flex flex-col gap-3 pt-10 md:self-end'>
 
-                <div className='flex items-center' onClick={() => { navigator.clipboard.writeText('chanjky97@gmail.com') }}>
+                <div className='flex items-center cursor-pointer w-fit' onClick={copyEvent}>
                     <div className='w-14 h-14 rounded-full bg-lime-400 border-2 border-white flex justify-center items-center'>
                         <MdEmail />
                     </div>
@@ -81,7 +72,7 @@ const ContactList = () => {
                 </div>
 
                 <Link passHref href="https://github.com/jasonchanhk">
-                    <div className='flex items-center'>
+                    <div className='flex items-center cursor-pointer w-fit'>
                         <div className='w-14 h-14 rounded-full bg-lime-400 border-2 border-white flex justify-center items-center'>
                             <FiGithub />
                         </div>
@@ -91,7 +82,7 @@ const ContactList = () => {
 
                 <Link passHref href="https://www.linkedin.com/in/jason-chan-361266217/">
 
-                    <div className='flex items-center'>
+                    <div className='flex items-center cursor-pointer w-fit'>
                         <div className='w-14 h-14 rounded-full bg-lime-400 border-2 border-white flex justify-center items-center'>
                             <FiLinkedin />
                         </div>
@@ -99,7 +90,7 @@ const ContactList = () => {
                     </div>
                 </Link>
 
-                <div className='flex items-center'>
+                <div className='flex items-center w-fit'>
                     <div className='w-14 h-14 rounded-full bg-lime-400 border-2 border-white flex justify-center items-center'>
                         <IoLocationSharp />
                     </div>
@@ -109,6 +100,64 @@ const ContactList = () => {
 
         </IconContext.Provider>
     )
+}
+
+const MessageForm = () => {
+    const form = useRef(null);
+    const [messageStatus, setMessageStatus] = useState<string>('')
+
+    const handleSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
+        event.preventDefault();
+
+        const currentForm = form.current;
+        if (currentForm == null) return;
+        if (process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID == null ||
+            process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID == null ||
+            process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY == null
+        ) return;
+
+        emailjs
+            .sendForm(
+                process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+                process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+                currentForm,
+                {
+                    publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+                })
+            .then(() => {
+                setMessageStatus('Success!');
+            })
+            .catch((error) => {
+                setMessageStatus('Failed...');
+                console.log(error.text)
+            }
+            );
+    }
+
+    return (
+        <form ref={form} onSubmit={handleSubmit}>
+            <div className='grid md:grid-cols-2 gap-5'>
+                <input type="text" placeholder='First Name' className='bg-gray-50 p-5 rounded-2xl' name='firstname' />
+                <input type="text" placeholder='Last Name' className='bg-gray-50 p-5 rounded-2xl' name='lastname' />
+                <input type="text" placeholder='Email' className='bg-gray-50 p-5 rounded-2xl' name='email' />
+                <input type="text" placeholder='Phone Number' className='bg-gray-50 p-5 rounded-2xl' name='phone' />
+                <input type="text" placeholder='Subject' className='bg-gray-50 col-span-2 p-5 rounded-2xl' name='subject' />
+                <textarea placeholder='Message' className='bg-gray-50 col-span-2 p-5 rounded-2xl h-52' name='message' />
+            </div>
+            <RedirectButton className='mt-10' coloured={true}>
+                {messageStatus != '' ? <div className='cursor-default w-32 text-center'>{messageStatus}</div> : <button type="submit" className='cursor-pointer w-32 text-center'>Send Message</button>}
+            </RedirectButton>
+        </form>
+    )
+}
+
+interface inputFormProps {
+    firstname?: string;
+    lastname?: string;
+    email?: string;
+    phone?: string;
+    subject?: string;
+    message?: string;
 }
 
 export default Contact
