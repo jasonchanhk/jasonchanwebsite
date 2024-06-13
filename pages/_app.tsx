a4f91e15b19d478b690410cbe5a0b45b08a95174
Jason Chan <jasonchan@Jasons-MacBook-Air.local>
2024-06-13 22:16:47 +0100

    try add google analytics & use hotjar react package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,46 @@
 import '../styles/globals.css'
+import Script from "next/script"
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
-import Hotjar from '@hotjar/browser';
+import { hotjar } from 'react-hotjar';
 import { useEffect } from 'react'
+
+interface hotjarOptionsProps {
+  id: number;
+  sv: number
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  const siteId = 5023668;
-  const hotjarVersion = 6;
+  const hotjarOptions: hotjarOptionsProps = {
+    id: Number(process.env.NEXT_PUBLIC_HOTJAR_SITEID),
+    sv: 6
+  }
 
   useEffect(() => {
-    Hotjar.init(siteId, hotjarVersion);
+    hotjar.initialize(hotjarOptions);
   }, [])
-  
+
   return (
     <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Component {...pageProps} />
       <Analytics />
     </>
